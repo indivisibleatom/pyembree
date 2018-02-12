4761619f9e72d429791dddc330e7b01e07a7a24f
indivisibleatom <mukulsati@gmail.com>
2018-02-11 20:04:16 -0500

    Add build system changes

--- setup.py
+++ setup.py
@@ -3,14 +3,16 @@
 from setuptools import setup, find_packages
 
 import numpy as np
+import os
 from Cython.Build import cythonize
 
-include_path = [np.get_include()]
+include_path = [np.get_include(),os.environ['DEV']+'/ext/build/include']
 
 ext_modules = cythonize('pyembree/*.pyx', language='c++',
                         include_path=include_path)
 for ext in ext_modules:
     ext.include_dirs = include_path
+    ext.library_dirs = [os.environ['DEV']+'/ext/build/lib']
     ext.libraries = ["embree"]
 
 setup(
